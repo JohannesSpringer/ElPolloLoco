de9c6cdab76a59b2b0ffbf8d5bd1c84339b3e785
JohannesSpringer <johannes.springer.01@gmx.de>
2023-03-17 14:24:43 +0100

    added animations for clouds, character, chickens

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,10 +2,19 @@ class Cloud extends MovableObject {
     y = 0;
     width = 1920 / 4;
     height = 1080 / 4;
+    x = canvas.width;
     
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
-        this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1;
+            if (this.x < -this.width) this.x = canvas.width;
+        }, 1000 / 60);
+        
     }
 }
