a53b3e6c3b8d438443b33e622d07bcbfee560586
JohannesSpringer <johannes.springer.01@gmx.de>
2023-03-18 19:15:23 +0100

    added endboss, gravity to character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     height = 1200 / 4;
     width = 610 / 4;
     x = 60;
+    y = 0;
     speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -12,26 +13,28 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    running_sound = new Audio('audio/running.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
-
-        this.y = canvasHeight - this.height - 48;
-
+        this.applyGravitiy();
         this.animate();
     }
 
     animate() {
         setInterval(() => {
+            this.running_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.running_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.running_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
@@ -39,10 +42,7 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 // walk animation
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 1000 / 20);
     }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,21 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = -2.5;
+
+    applyGravitiy() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y += this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 130;
+    }
 
     loadImage(path) {
         this.img = new Image();
@@ -24,6 +39,13 @@ class MovableObject {
             img.src = path;
             this.imageCache[path] = img;
         })
+    }
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 
     moveRight() {
