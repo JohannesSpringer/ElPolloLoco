57014ecc0a85133df7530604180e268e84dd4cd6
Johannes Springer <johannes.springer.01@gmx.de>
2023-06-26 15:51:49 +0200

    add: smartphone buttons

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -6,4 +6,72 @@ class Keyboard {
     SPACE = false;
     CTRL = false;
     D = false;
+
+    constructor() {
+        this.bindKeyPressEvents();
+    }
+
+    bindBtsPressEvents() {
+        document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.LEFT = true;
+        });
+
+        document.getElementById('btnLeft').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.LEFT = false;
+        });
+
+        document.getElementById('btnRight').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.RIGHT = true;
+        });
+
+        document.getElementById('btnRight').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.RIGHT = false;
+        });
+
+        document.getElementById('btnJump').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.SPACE = true;
+        });
+
+        document.getElementById('btnJump').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.SPACE = false;
+        });
+
+        document.getElementById('btnThrow').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.CTRL = true;
+        });
+
+        document.getElementById('btnThrow').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.CTRL = false;
+        });
+    }
+
+    bindKeyPressEvents() {
+        window.addEventListener("keydown", (event) => {
+            if (event.keyCode == 17) keyboard.CTRL = true;
+            if (event.keyCode == 32) keyboard.SPACE = true;
+            if (event.keyCode == 37) keyboard.LEFT = true;
+            if (event.keyCode == 38) keyboard.UP = true;
+            if (event.keyCode == 39) keyboard.RIGHT = true;
+            if (event.keyCode == 40) keyboard.DOWN = true;
+            if (event.keyCode == 68) keyboard.D = true;
+        });
+
+        window.addEventListener("keyup", (event) => {
+            if (event.keyCode == 17) keyboard.CTRL = false;
+            if (event.keyCode == 32) keyboard.SPACE = false;
+            if (event.keyCode == 37) keyboard.LEFT = false;
+            if (event.keyCode == 38) keyboard.UP = false;
+            if (event.keyCode == 39) keyboard.RIGHT = false;
+            if (event.keyCode == 40) keyboard.DOWN = false;
+            if (event.keyCode == 68) keyboard.D = false;
+        });
+    }
 }
